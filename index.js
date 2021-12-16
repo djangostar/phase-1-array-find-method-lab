29ffdc084538a4563f38a5fc81816d380f5e0081
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-12-16 12:21:50 -0800

    commit tree df1d5b6e0cf3dd79b5f416389dd0a8b084936e62

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+//DECLARATION
+//function superbowlWin(records) {
+//    const win = records.find(records => records.result === "W")
+//    return win ? win.year : undefined
+//}
+
+const superbowlWin = (records) => {
+    const win = records.find(records => records.result === "W")
+    return win ? win.year : undefined
+}
